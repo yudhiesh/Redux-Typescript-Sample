c88510c2db714039517c3a81406b1b6c62703ee8
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-04-01 22:38:36 +0800

    Added Notification Slice

--- src/features/notifications/notificationsSlice.ts
+++ src/features/notifications/notificationsSlice.ts
@@ -1,36 +1,37 @@
-import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { client } from "../../api/client";
 import { RootState } from "../../app/store";
 import { Notifications } from "../../types/types";
 
 export const selectAllNotifications = (state: RootState) => state.notifications;
 
-export const fetchNotifications = createAsyncThunk(
-  "notifications/fetchNotifications",
-  async (_, { getState }) => {
-    const allNotifications = selectAllNotifications(getState());
-    const [latestNotifications] = allNotifications;
-    const latestTimestamp = latestNotifications ? latestNotifications.date : "";
-    const response = await client.get(
-      `api/notifications?since=${latestTimestamp}`
-    );
-    return response.notifications;
-  }
-);
+export const fetchNotifications = createAsyncThunk<
+  Notifications,
+  void,
+  { state: RootState }
+>("notifications/fetchNotifications", async (_, thunkAPI) => {
+  const { getState } = thunkAPI;
+  const allNotifications = selectAllNotifications(getState());
+  const [latestNotification] = allNotifications;
+  const latestTimestamp = latestNotification ? latestNotification.date : "";
+  const response = await client.get(
+    `/api/notifications?since=${latestTimestamp}`
+  );
+  return response.notifications as Notifications;
+});
 
+// Initial State for the notifications
+const initialState: Notifications[] = [];
 
 const notificationSlice = createSlice({
-  name: "notification",
-  [] as Notifications,
+  name: "notifications",
+  initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(
-      fetchNotifications.fulfilled,
-      (state, action) => {
-        state.push(action.payload);
-        state.sort((a, b) => b.date.localeCompare(a.date));
-      }
-    );
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.push(action.payload);
+      state.sort((a, b) => b.date.localeCompare(a.date));
+    });
   },
 });
 
